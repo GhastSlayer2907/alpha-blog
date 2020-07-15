3f55bfe0c3b3270297fef2cb7943ed544060e607
Saker Alabas <salabas2907@gmail.com>
2020-07-15 18:47:09 +0400

    Created new, create, delete, and update routes for articles table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+
+  #Create routes for imteracting with the articles table
+  resources :articles
 end
